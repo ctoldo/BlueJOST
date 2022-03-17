2de2ea75db35e1c7114cd9a073e4aa081c15b385
ctoldo <claudio.toldo00@gmail.com>
2022-03-17 14:01:03 +0100

    Student Woche 4

---
+++ src/main/java/ch/ost/toldo/chapter4/student/Lehrgang.java
@@ -0,0 +1,33 @@
+/**
+ * Author: Claudio Toldo
+ * Created: 10.03.2022 13:54
+ * © 2022 Claudio Toldo
+ */
+package ch.ost.toldo.chapter4.student;
+
+public class Lehrgang {
+
+  private String name;
+  private double preis;
+
+  public Lehrgang(String name) {
+    this.name = name;
+  }
+
+  public Lehrgang(String name, double preis) {
+    this(name);
+    this.preis = preis;
+  }
+
+  public String gibName() {
+    return name;
+  }
+
+  public double gibPreis() {
+    return preis;
+  }
+
+  public void setzePreis(double preis) {
+    this.preis = preis;
+  }
+}
