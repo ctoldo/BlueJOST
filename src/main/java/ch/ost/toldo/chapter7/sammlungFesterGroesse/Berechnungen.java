1f2d3a01a0cb8fb06cb20dd2466b1eb4f10aa45b
ctoldo <claudio.toldo00@gmail.com>
2022-03-31 14:42:28 +0200

    Sammlung fester Grössen Woche 6

---
+++ src/main/java/ch/ost/toldo/chapter7/sammlungFesterGroesse/Berechnungen.java
@@ -0,0 +1,74 @@
+/*
+ * Author: Claudio Toldo
+ * Created: 31.03.2022 14:04
+ * © 2022 Claudio Toldo
+ */
+package ch.ost.toldo.chapter7.sammlungFesterGroesse;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Berechnungen {
+
+  /**
+   * berechnet die Fakultät für die übergebene Zahl
+   * und gibt die Rechnung und die Zwischenresultate aus.
+   *
+   * @param zahl Zahl von der die Fakultät berechnet werden soll
+   */
+  public static void ausgebenFakultaet(int zahl) {
+    long fakultaet = 1;
+    long[] zwischenresultate = new long[zahl];
+    for (int i = 1; i <= zahl; i++) {
+      System.out.print(i);
+      fakultaet *= i;
+      zwischenresultate[i - 1] = fakultaet;
+      if (i < zahl) {
+        System.out.print(" x ");
+      }
+    }
+    System.out.println(" = " + fakultaet);
+
+    for (long zwischenresultat : zwischenresultate) {
+      System.out.println(zwischenresultat);
+    }
+  }
+
+  /**
+   * berechnet den Druchmesser, den Kreisumfang und die Kreisfläche
+   * für den gegebenen Radius und gibt diese aus.
+   *
+   * @param radius radius, auf dem die Berechnungen basieren sollen
+   */
+  public static void kreisBerechnungen(double radius) {
+    double durchmesser = radius * 2;
+    double kreisumfang = durchmesser * Math.PI;
+    double kreisflaeche = Math.pow(radius, 2) * Math.PI;
+    System.out.println("Durchmesser:\t" + durchmesser);
+    System.out.println("Kreisumfang:\t" + kreisumfang);
+    System.out.println("Kreisfläche:\t" + kreisflaeche);
+  }
+
+  /**
+   * generiert Lottozahlen zwischen 1 und maxWert (inklusive)
+   *
+   * @param anzahl   Anzahl Lottozahlen
+   * @param maxWert  Höchster möglicher Zahlenwert
+   * @param ausgeben ob, die Lottozahlen ausgegeben werden sollen?
+   * @return lottozahlen
+   */
+  public static ArrayList<Integer> gibLottozahlen(int anzahl, int maxWert, boolean ausgeben) {
+    ArrayList<Integer> lottozahlen = new ArrayList<>();
+    Random random = new Random();
+    for (int i = 0; i < anzahl; i++) {
+      lottozahlen.add(random.nextInt(1, maxWert + 1));
+    }
+
+    if (ausgeben) {
+      for (Integer lottozahl : lottozahlen) {
+        System.out.println(lottozahl);
+      }
+    }
+    return lottozahlen;
+  }
+}
---
+++ src/main/java/ch/ost/toldo/chapter7/sammlungFesterGroesse/MainClass.java
@@ -0,0 +1,14 @@
+/*
+ * Author: Claudio Toldo
+ * Created: 31.03.2022 14:11
+ * © 2022 Claudio Toldo
+ */
+package ch.ost.toldo.chapter7.sammlungFesterGroesse;
+
+public class MainClass {
+  public static void main(String[] args) {
+    Berechnungen.ausgebenFakultaet(10);
+    //Berechnungen.kreisBerechnungen(5);
+    //Berechnungen.gibLottozahlen(6, 30, true);
+  }
+}
